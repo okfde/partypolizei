76d76b5844ded0a930186a403a2c6a7d4f32ffe7
Knut Hühne <knut@k-nut.eu>
2017-07-27 12:10:24 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+    return gulp.src('sass/main.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(reload({ stream:true }));
+});
+
+// watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
+gulp.task('serve', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        }
+    });
+
+    gulp.watch('sass/*.sass', ['sass']);
+    gulp.watch(['*.html'], {cwd: 'app'}, reload);
+});
